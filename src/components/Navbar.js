31b6d05e4c1441838987a78c21175fcf20b78fb3
Jordan Masone <jordanmasone1@gmail.com>
2021-05-08 10:45:48 -0400

    Initial file creation and bootstrap linking.

---
+++ src/components/Navbar.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function Navbar() {
+    return (
+        <nav class="navbar navbar-light bg-light">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="/">
+                    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/21/Eo_circle_pink_letter-p.svg/480px-Eo_circle_pink_letter-p.svg.png" alt="" width="30" height="30" className="d-inline-block align-text-top" />
+                        Password Generator
+                </a>
+            </div>
+        </nav>
+    )
+}
